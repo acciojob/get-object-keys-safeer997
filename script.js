ec2e145f6e4257b1b7efc078695c0dad16c9e5a0
safeer997 <143935408+safeer997@users.noreply.github.com>
2025-03-09 01:06:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// Create the student object
+const student = {
+  name: "John",
+};
+
+// Function to get all keys from the object
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+// Export the function if required for Cypress testing (if running in Node.js)
+if (typeof module !== "undefined") {
+  module.exports = { student, getKeys };
+}
